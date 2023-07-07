c1f88b66911a9c4b88babed8b1ea82c347187dca
tanvir <shovontanvir124@gmail.com>
2023-07-07 17:29:16 +0600

    added restriction on updateRole route that without admin no one can access the route

--- routes/userRouter..js
+++ routes/userRouter..js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { updateRole } = require("../controllers/userController");
+const { adminAuth } = require("../middleware/auth");
 
-router.patch("/updateRole", updateRole);
+router.patch("/updateRole", adminAuth(["admin"]), updateRole);
 
 module.exports = router;
