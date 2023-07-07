10e47b57cceff664ccbbeb3b1e43677065aefb78
tanvir <shovontanvir124@gmail.com>
2023-07-07 17:03:29 +0600

    added logout route and logout controller function

--- controllers/authController.js
+++ controllers/authController.js
@@ -132,3 +132,11 @@ exports.refreshToken = (req, res) => {
   );
 };
 
+
+exports.logout = (req, res) => {
+  const cookies = req.cookies;
+  if (!cookies?.jwt) return res.sendStatus(204); //No content
+  res.clearCookie("jwt", { httpOnly: true, sameSite: "None", secure: true });
+  res.json({ message: "Cookie cleared" });
+};
+
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -3,10 +3,12 @@ const {
   register,
   login,
   refreshToken,
+  logout
 } = require("../controllers/authController");
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/refreshToken", refreshToken);
+router.post("/logout", logout);
 
 module.exports = router;
